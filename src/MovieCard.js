5ea0caa73fce3799826d1c00b18aedfa28f5a7db
Ian Bachiller <ianbachise@gmail.com>
2024-03-29 17:50:26 -0400

    movie card and movie list working; iterating each movie card on the dom

--- src/MovieCard.js
+++ src/MovieCard.js
@@ -1,17 +1,16 @@
 import React, { useContext } from "react";
 
-function MovieCard() {
+function MovieCard({id, image, title}) {
   return (
     <div>
       <li className="card" data-testid="movie-item">
-        <img/>
-        <h4></h4>
-        <p></p>
-        <button>
-        </button>
-        <button>
-        </button>
+        <img 
+            src = {image}
+            alt={image}/>
+        <h4>{title}</h4>
+        <p>{title}</p>
       </li>
     </div>
   );
 }
+export default MovieCard
--- src/MovieList.js
+++ src/MovieList.js
@@ -1,10 +1,18 @@
 import React from "react";
+import MovieCard from "./MovieCard"
 
 function MovieList ({displayMovies}) {
     console.log("From MovieList:", displayMovies)
+    const iterateMovies = displayMovies.map(movie => <MovieCard 
+        key={movie.id}
+        id={movie.id}
+        title={movie.title}
+        image={movie.image}
+        
+         />)
     return (
         <div>
-
+            {iterateMovies}
         </div>
     )
 }
