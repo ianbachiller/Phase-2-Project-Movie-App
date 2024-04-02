5e88cd519b5d209b9dfd599eb87e8da07696a82f
Ian Bachiller <ianbachise@gmail.com>
2024-04-02 01:10:43 -0400

    added handle cb functions for change listeners to input forms for new movie

--- src/NavBar.js
+++ src/NavBar.js
@@ -5,7 +5,7 @@ function NavBar() {
   return (
     <div className="navbar">
       <h1 className="title">Movie Night!</h1>
-      <h1 className="title">Where the movie with the most oscar-likes wins!</h1>
+      <h2 className="title">Where the movie with the most oscar-likes wins!</h2>
       <NewMovieForm />
       <input placeholder="Search movie"></input>
     </div>
--- src/NewMovieForm.js
+++ src/NewMovieForm.js
@@ -1,15 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
 
 function NewMovieForm() {
+  const [title, setTitle] = useState("");
+  const [poster, setPoster] = useState("");
+  const [plot, setPlot] = useState("");
+  const [newMovie, setNewMovie] = useState([]);
+  const [genre, setGenre] = useState([]);
+  const genres = [
+    "Action",
+    "Adventure",
+    "Comedy",
+    "Drama",
+    "Fantasy",
+    "Sci-Fi",
+    "Other",
+  ];
+  function handleTitleChange(e) {
+    setTitle(e.target.value);
+  }
+  function handlePosterChange(e) {
+    setPoster(e.target.value);
+  }
+  function handlePlotChange(e) {
+    setPlot(e.target.value);
+  }
+  function handleGenresChange(e) {
+    const selectedOptions = Array.from(e.target.selectedOptions, option => option.value)
+    setGenre(selectedOptions)
+  }
+  function handleNewMovieSubmit(e) {
+    e.preventDefault();
+    const submittedMovie = {
+      title: title,
+      genre: genre,
+      plotSummary: plot,
+      image: poster,
+    };
+    setTitle("");
+    setPlot("");
+    setPoster("");
+    setGenre([])
+    setNewMovie(submittedMovie);
+    console.log("Final submitted movie:",submittedMovie);
+  }
   return (
-    <div className="new-movie-form">
-      <form>
-        <input type="text" name="name" placeholder="Movie name" />
-        <input type="text" name="image" placeholder="Image URL" />
-        <input type="text" name="genre" placeholder="Genre" />
-        <input type="text" name="plotSummary" placeholder="Plot Summary" />
-        <button type="submit">Add Movie</button>
-      </form>
+    <div>
+      <div className="new-movie-form">
+        <form onSubmit={handleNewMovieSubmit}>
+          <input
+            type="text"
+            name="name"
+            placeholder="Movie name"
+            onChange={handleTitleChange}
+            value={title}
+          />
+          <input
+            type="text"
+            name="image"
+            placeholder="Poster URL"
+            onChange={handlePosterChange}
+            value={poster}
+          />
+          <input
+            type="text"
+            name="plotSummary"
+            placeholder="Plot Summary"
+            onChange={handlePlotChange}
+            value={plot}
+          />
+          <select multiple onChange={handleGenresChange}>
+            {genres.map((genre) => (
+              <option key={genre} value={genre}>
+                {genre}
+              </option>
+            ))}
+          </select>
+          <button type="submit">Add Movie</button>
+        </form>
+      </div>
     </div>
   );
 }
