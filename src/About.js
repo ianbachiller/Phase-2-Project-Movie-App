90976d5b302b98e86fd0a1c5b2d8019bb09f0adf
Ian Bachiller <ianbachise@gmail.com>
2024-04-06 16:56:20 -0400

    added styling

--- src/About.js
+++ src/About.js
@@ -5,11 +5,13 @@ import Header from "./Header";
 function About() {
   return (
     <div>
-        <Header />
-        <NavBar />
+      <Header />
+      <NavBar />
       <div className="about">
-        Have you and your friends or family been stuck on choosing which movie
-        to watch on movie night? Well, fret not! This app is for you!
+        <p>
+          Have you and your friends or family been stuck on choosing which movie
+          to watch on movie night? Well, fret not! This app is for you!
+        </p>
       </div>
     </div>
   );
